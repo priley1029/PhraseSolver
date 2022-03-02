dce950f752cc7dd8e41b4ca82982f3bbaeaa1fe3
priley1029 <94567488+priley1029@users.noreply.github.com>
2022-03-02 00:07:19 -0800

    Add files via upload

---
+++ PhraseSolver.java
@@ -0,0 +1,106 @@
+import java.util.Scanner;
+import java.io.File;
+
+public class PhraseSolver
+{
+  private String phrase = "";
+  private String solvedPhrase = "";
+  private int currentLetterValue; 
+
+
+  public String getPhrase()
+  {
+    return phrase;
+  }
+  public String getSolvedPhrase()
+  {
+    return solvedPhrase;
+  }
+  public int getLetterValue()
+  {
+    return currentLetterValue;
+  }
+ 
+  public void setLetterValue()
+  {
+    int randomInt = (int) ((Math.random() * 10) + 1) * 100;    
+    currentLetterValue = randomInt;
+  }
+
+  public boolean isSolved(String guess)
+  {
+    if (phrase.equals(guess))
+    {
+      return true;
+    }
+    return false;
+  }
+
+  private String loadPhrase()
+  {
+    String tempPhrase = "";
+    
+    int numOfLines = 0;
+    try 
+    {
+      Scanner sc = new Scanner(new File("phrases.txt"));
+      while (sc.hasNextLine())
+      {
+        tempPhrase = sc.nextLine().trim();
+        numOfLines++;
+      }
+    } catch(Exception e) { System.out.println("Error reading or parsing phrases.txt"); }
+    
+		int randomInt = (int) ((Math.random() * numOfLines) + 1);
+    
+    try 
+    {
+      int count = 0;
+      Scanner sc = new Scanner(new File("phrases.txt"));
+      while (sc.hasNextLine())
+      {
+        count++;
+        String temp = sc.nextLine().trim();
+        if (count == randomInt)
+        {
+          tempPhrase = temp;
+        }
+      }
+    } catch (Exception e) { System.out.println("Error reading or parsing phrases.txt"); }
+    
+    for (int i = 0; i < tempPhrase.length(); i++)
+    {
+      if (tempPhrase.substring(i, i + 1).equals(" "))
+      {
+        solvedPhrase += "  ";
+      }  
+      else
+      {
+        solvedPhrase += "_ ";
+      }
+    }  
+    
+    return tempPhrase;
+  }  
+
+  public boolean guessLetter(String guess)
+  {
+    boolean foundLetter = false;
+    String newSolvedPhrase = "";
+    
+    for (int i = 0; i < phrase.length(); i++)
+    {
+      if (phrase.substring(i, i + 1).equals(guess))
+      {
+        newSolvedPhrase += guess + " ";
+        foundLetter = true;
+      }
+      else
+      {
+        newSolvedPhrase += solvedPhrase.substring(i * 2, i * 2 + 1) + " ";  
+      }
+    }
+    solvedPhrase = newSolvedPhrase;
+    return foundLetter;
+  } 
+} 
